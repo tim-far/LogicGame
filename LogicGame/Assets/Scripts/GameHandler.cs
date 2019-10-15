623b67faddd56915cb0bd13b2c6668a3860147e0
Tim Farahani <tfa@jcbachmann.de>
2019-10-15 14:48:19 +0200

    Add Single touch features
    Currently a white square is following the position of the finger touching the screen.

---
+++ LogicGame/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public GameObject ob;
+    public GameObject player;
+
+    private Utility utility;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Started GameHandler");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch t = Input.GetTouch(0);
+
+            if (t.phase == TouchPhase.Began)
+            {
+                player = createGameObject(t);
+            }
+            else if (t.phase == TouchPhase.Ended)
+            {
+               Destroy(player);
+            }
+            else if (t.phase == TouchPhase.Moved && ob)
+            {
+                player.transform.position = getTouchPosition(t.position);
+            }
+        }
+     
+    }
+
+    Vector2 getTouchPosition(Vector2 touchPosition)
+    {
+        return GetComponent<Camera>().ScreenToWorldPoint(new Vector3(touchPosition.x, touchPosition.y, 0));
+    }
+
+    GameObject createGameObject(Touch t)
+    {
+        GameObject c = Instantiate(ob) as GameObject;
+        c.name = "test";
+        c.transform.position = getTouchPosition(t.position);
+        return c;   
+    }
+}
---
+++ LogicGame/Assets/TouchPosition.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchPosition : MonoBehaviour
+{
+    public GameObject gobj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch t = Input.GetTouch(0);
+            if(t.phase == TouchPhase.Began)
+            {
+                Debug.Log("Touch Began");
+                gobj = createCircle();
+            }
+            else if (t.phase == TouchPhase.Ended)
+            {
+                Debug.Log("Touch Ended");
+                Destroy(gobj);
+            }
+        }
+    }
+
+    GameObject createCircle()
+    {
+        GameObject tmp = Instantiate(gobj) as GameObject;
+        tmp.name = "Circle";
+        tmp.transform.position = transform.position;
+        return tmp;
+    }
+}
